15321d704dac96ac15768018e2d9284ac1d3a22f
Femi Elice <femisimbolon00@gmail.com>
2020-11-18 10:37:24 +0700

    create main

---
+++ kuis/src/view/mainKuis.java
@@ -0,0 +1,22 @@
+package View;
+
+
+import View.inputDataMahasiswa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author A442UF
+ */
+public class mainKuis {
+    public static void main(String[] args) {
+        new inputDataMahasiswa();
+    }
+    
+}
